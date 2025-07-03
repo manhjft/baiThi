f214721abcb9b71676afe90fc6ee9a313cf27b3b
manhjft <nhammanh022@gmail.com>
2025-07-03 18:11:09 +0700

    bài thi 1

---
+++ Exercise1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+    printf("input n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    printf("The sum from 1 to %d is: %d\n", n, sum);
+   
+}
+
